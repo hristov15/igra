2e5fd957a09815315561971514b31ed7778ebc6c
Aleksandar <alexHHHristov@gmail.com>
2023-02-20 14:29:07 +0200

    adfa

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
-let nx = 4;
-let ny = 3;
+let nx = 8;
+let ny = 8;
 let sqside = 50;
 let herox = 2;
 let heroy = 1;
 let heroimg = new Image();
 heroimg.src = "snimka.png";
 canvas.width = nx * sqside;
-canvas.hight = ny * sqside;
+canvas.height = ny * sqside;
 function drawMap() {
+	context.clearRect(0,0,canvas.width,canvas.height);
 	context.drawImage(heroimg, herox * sqside, heroy * sqside, sqside, sqside);
 	for ( let i = 0; i < nx; i++){
 		for ( let j = 0; j < ny; j++){
